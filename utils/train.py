df66f33175c2210545cff9a8630e910deaa3fff7
adkevin3307 <adkevin3307@gmail.com>
2021-06-25 08:42:57 +0800

    reformat output message

--- utils/train.py
+++ utils/train.py
@@ -1,21 +1,15 @@
-import torch
-
-import numpy as np
-
 from tqdm import tqdm
+import torch
 
 
 def train(args, trainloader, testloader, net, optim, loss_func):
-
     for epoch in range(args.epochs):
-
         net.train()
 
         train_loss = 0
         train_acc = 0
 
         for img, label in tqdm(trainloader):
-
             img = img.to(args.device)
             label = label.to(args.device)
 
@@ -33,21 +27,21 @@ def train(args, trainloader, testloader, net, optim, loss_func):
 
             loss.backward()
             optim.step()
-        print(f'Train Loss: {train_loss/len(trainloader)},'
-              f'Acc: {train_acc/len(trainloader.dataset)}')
+
+        print(f'Epoch {epoch + 1}, Loss: {(train_loss / len(trainloader)):.3f}, Accuracy: {(train_acc / len(trainloader.dataset)):.3f}')
 
         test(args, testloader, net, loss_func)
 
-        if(epoch % 10 == 9):
-            torch.save(net, f'checkpoint/vgg19_{epoch}.pt')
+        if (epoch + 1) % 1 == 0:
+            torch.save(net, f'checkpoint/vgg19_{epoch + 1}.pt')
 
 
 def test(args, testloader, net, loss_func):
-
     net.eval()
 
     test_loss = 0
     test_acc = 0
+
     for img, label in testloader:
         img = img.to(args.device)
         label = label.to(args.device)
@@ -61,5 +55,5 @@ def test(args, testloader, net, loss_func):
         test_correct = (pred == label).sum()
 
         test_acc += test_correct
-    print(f'Test Loss: {test_loss/len(testloader)},'
-          f'Acc: {test_acc/len(testloader.dataset)}')
+
+    print(f'Test, Loss: {(test_loss / len(testloader)):.3f}, Accuracy: {(test_acc / len(testloader.dataset)):.3f}')
